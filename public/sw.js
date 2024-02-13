09bb465a6c513e4ac91768c0728e81a19a39a1e9
oghomena <oghomenag@gmail.com>
2024-02-13 14:27:50 +0100

    initial

---
+++ public/sw.js
@@ -0,0 +1,37 @@
+console.warn("sw file in pub");
+
+let cacheData = "appv1";
+
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+      cache.addAll([
+        "/static/js/main.chunks.js",
+        "/static/js/bundle.js",
+        "/index.html",
+        "/users",
+        "/images/store.png",
+        "/images/bell.png",
+        "/images/settings.png",
+        "/images/pen.png",
+        "/components/BottomNav",
+        "/",
+      ]);
+    })
+  );
+});
+
+this.addEventListener("fetch", (event) => {
+  if (!navigator.onLine) {
+    event.respondWith(
+      caches.match(event.request).then((res) => {
+        if (res) {
+          return res;
+        }
+
+        let requestUrl = event.request.clone();
+        fetch(requestUrl);
+      })
+    );
+  }
+});
